5c004a3c37f7c1923253bfe7f6e4c04b1bfe9534
Gabriel Dumitrescu <gdumitrescu@users.noreply.github.com>
2014-01-13 11:17:49 -0500

    Add lesson 3

---
+++ lesson3/js/app.js
@@ -0,0 +1,33 @@
+(function(Apigee){
+
+  var client = new Apigee.Client({
+    orgName: "gabriel", // Your Apigee.com username for App Services
+    appName: "sandbox" // Your Apigee App Services app name
+  });
+
+  // Reading data
+  var books = new Apigee.Collection({
+    "client": client,
+    "type":"books"
+  });
+
+  $(document).on("pageshow", "#books", function() {
+    books.fetch(
+      function(err, data) { // Success
+        if (err) {
+          alert("read failed");
+        } else {
+          while(books.hasNextEntity()) {
+            var book = books.getNextEntity();
+            var title = book.get("title");
+            var author = book.get("author");
+            // Output the books
+            $(".items-list").append("<li><h3>"+title+"</h3><p>"+author+"</p></li>");
+          }
+          // Refresh list to apply jQuery Mobile styles
+          $(".items-list").listview("refresh");
+        }
+    });
+  });
+
+})(Apigee);
