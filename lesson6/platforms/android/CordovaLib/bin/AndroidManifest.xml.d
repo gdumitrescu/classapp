747d0b3e15623fe9d113740bc22c646424611c8c
Gabriel Dumitrescu <gdumitrescu@users.noreply.github.com>
2014-02-19 03:46:02 -0500

    Add lesson 6

---
+++ lesson6/platforms/android/CordovaLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/gabriel/GitHub/classapp/lesson6/platforms/android/CordovaLib/bin/AndroidManifest.xml : \
+/home/gabriel/GitHub/classapp/lesson6/platforms/android/CordovaLib/AndroidManifest.xml \
